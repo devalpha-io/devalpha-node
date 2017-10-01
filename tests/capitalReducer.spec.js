1d0c51197ba2565ed720e93805a14a16f3ec1872
Filip Hallqvist <filiphallqvist@gmail.com>
2017-10-01 17:04:29 +0200

    Added tests for filled orders in capitalReducer

--- tests/capitalReducer.spec.js
+++ tests/capitalReducer.spec.js
@@ -21,7 +21,7 @@ test('return the initial state', (t) => {
   t.deepEqual(actual.toJS(), expect.toJS())
 })
 
-test(`${ORDER_PLACED} of a sell-side order does not modify anything`, (t) => {
+test(`${ORDER_PLACED} of a sell-side order correctly edits reservedCash`, (t) => {
   const order = {
     id: '1',
     identifier: 'MSFT',
@@ -32,7 +32,11 @@ test(`${ORDER_PLACED} of a sell-side order does not modify anything`, (t) => {
   const action = { type: ORDER_PLACED, payload: order }
 
   const actual = reducer(undefined, action)
-  const expect = reducer(undefined, {})
+  const expect = Map({
+    reservedCash: 5.5,
+    cash: -5.5,
+    commission: 0
+  })
 
   t.true(is(actual, expect))
 })
@@ -94,6 +98,174 @@ test(`${ORDER_CANCELLED} of a buy-side order correctly reverts cash and reserved
     reservedCash: 0,
     commission: 0
   }))
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, sell-side, should increase cash and commission, and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: -100,
+    price: 100,
+    commission: 10,
+    expectedQuantity: -100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: -10,
+    reservedCash: 10,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 9990,
+    reservedCash: 0,
+    commission: 10
+  })
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, buy-side, should increase commission and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: 100,
+    price: 100,
+    commission: 10,
+    expectedQuantity: 100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: 0,
+    reservedCash: 10010,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 0,
+    reservedCash: 0,
+    commission: 10
+  })
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, buy-side, partial fill, should increase commission and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: 50,
+    price: 100,
+    commission: 5,
+    expectedQuantity: 100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: 0,
+    reservedCash: 10010,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 0,
+    reservedCash: 5005,
+    commission: 5
+  })
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, sell-side, partial fill, increase cash and commission, and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: -50,
+    price: 100,
+    commission: 5,
+    expectedQuantity: -100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: -10,
+    reservedCash: 10,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 4990,
+    reservedCash: 5,
+    commission: 5
+  })
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, buy-side, better price, should increase commission and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: 100,
+    price: 90,
+    commission: 9,
+    expectedQuantity: 100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: 0,
+    reservedCash: 10010,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 0,
+    reservedCash: 0,
+    commission: 9
+  })
+
+  t.true(is(actual, expect))
+})
+
+test(`${ORDER_FILLED}, sell-side, better price, increase cash and commission, and decrease reservedCash`, (t) => {
+  const order = {
+    id: '0',
+    identifier: 'MSFT',
+    quantity: -100,
+    price: 110,
+    commission: 11,
+    expectedQuantity: -100,
+    expectedPrice: 100,
+    expectedCommission: 10
+  }
+  const action = { type: ORDER_FILLED, payload: order }
+  const initialState = Map({
+    cash: -10,
+    reservedCash: 10,
+    commission: 0
+  })
+
+  const actual = reducer(initialState, action)
+  const expect = Map({
+    cash: 10979,
+    reservedCash: 0,
+    commission: 11
+  })
 
   t.true(is(actual, expect))
 })
--- tests/rootReducer.spec.js
+++ tests/rootReducer.spec.js
@@ -1,16 +1,66 @@
+/**
+ * We use this file to test the globalReducer, as it is strongly dependent on the other reducers,
+ * and as such there is no point in testing it on its own.
+ */
+
 import test from 'ava'
 import sinon from 'sinon'
 import { Map, List, is } from 'immutable'
 
-import reducer from '../lib/reducers/capitalReducer'
+import reducer from '../lib/reducers'
 import {
   INITIALIZED,
   ORDER_FILLED,
   BAR_RECEIVED
 } from '../lib/constants'
 
-/**
- * We use this file to test the globalReducer, as it is strongly dependent on the other reducers,
- * and as such there is no point in testing it on its own.
- */
-test.todo('return the initial state')
+test('return the initial state', (t) => {
+  const actual = reducer(undefined, {})
+  const expect = Map({
+    capital: Map({
+      cash: 0,
+      commission: 0,
+      reservedCash: 0
+    }),
+    positions: Map(),
+    orders: Map(),
+    history: List(),
+    metrics: Map({
+      maxDrawdown: 0,
+      sharpeRatio: 0
+    }),
+    returnsTotal: 0,
+    returnsPeriod: 0,
+    total: 0,
+    timestamp: 0
+  })
+  t.true(is(actual, expect))
+})
+
+test(`${INITIALIZED} correctly builds the first history`, (t) => {
+  const action = { type: INITIALIZED, payload: { timestamp: 100 } }
+  const actual = reducer(undefined, action).get('history').get(0)
+  const expect = Map({
+    capital: Map({
+      cash: 0,
+      commission: 0,
+      reservedCash: 0
+    }),
+    positions: Map(),
+    orders: Map(),
+    metrics: Map({
+      maxDrawdown: 0,
+      sharpeRatio: 0
+    }),
+    returnsTotal: 0,
+    returnsPeriod: 0,
+    total: 0,
+    timestamp: 100
+  })
+
+  t.true(is(actual, expect))
+})
+
+test.todo(`${ORDER_FILLED} updates total as well as returns`)
+
+test.todo(`${BAR_RECEIVED} updates total as well as returns`)
