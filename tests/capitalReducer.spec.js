2a2e336210daf4314ff0c6da4da5df2688360604
Filip Hallqvist <filiphallqvist@gmail.com>
2017-12-04 15:53:25 +0100

    Removed history from capitalReducer

--- tests/capitalReducer.spec.js
+++ tests/capitalReducer.spec.js
@@ -15,24 +15,9 @@ test('return the initial state', (t) => {
   const expect = Map({
     cash: 0,
     commission: 0,
-    reservedCash: 0,
-    history: List()
+    reservedCash: 0
   })
   t.deepEqual(actual.toJS(), expect.toJS())
-})
-
-test(`add initial state to history on ${INITIALIZED}`, (t) => {
-  const initialState = reducer(undefined, {})
-  const action = { type: INITIALIZED, payload: { timestamp: 100, initialStates: {} } }
-
-  const actual = reducer(undefined, action).get('history')
-  const expect = List([
-    initialState
-      .set('timestamp', 100)
-      .delete('history')
-  ])
-
-  t.true(is(actual, expect))
 })
 
 test(`set initial values on ${INITIALIZED}`, (t) => {
@@ -44,8 +29,7 @@ test(`set initial values on ${INITIALIZED}`, (t) => {
         capital: {
           cash: 100,
           reservedCash: 101,
-          commission: 102,
-          history: ['foobar']
+          commission: 102
         }
       }
     }
@@ -55,16 +39,7 @@ test(`set initial values on ${INITIALIZED}`, (t) => {
   const expect = Map({
     cash: 100,
     reservedCash: 101,
-    commission: 102,
-    history: List([
-      'foobar',
-      Map({
-        cash: 100,
-        reservedCash: 101,
-        commission: 102,
-        timestamp: 50
-      })
-    ])
+    commission: 102
   })
 
   t.true(is(actual, expect))
@@ -84,8 +59,7 @@ test(`${ORDER_PLACED} of a sell-side order correctly edits reservedCash`, (t) =>
   const expect = Map({
     reservedCash: 5.5,
     cash: -5.5,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   t.true(is(actual, expect))
@@ -105,8 +79,7 @@ test(`${ORDER_PLACED} of a buy-side order correctly edits cash, commission and r
   const expect = reducer(undefined, {}).merge(Map({
     reservedCash: 10010,
     cash: -10010,
-    commission: 0,
-    history: List()
+    commission: 0
   }))
 
   t.true(is(actual, expect))
@@ -118,8 +91,7 @@ test(`${ORDER_CANCELLED} of a sell-side order does not modify anything`, (t) => 
     identifier: 'MSFT',
     quantity: -50,
     price: 110,
-    commission: 5.5,
-    history: List()
+    commission: 5.5
   }
   const action = { type: ORDER_CANCELLED, payload: order }
 
@@ -141,8 +113,7 @@ test(`${ORDER_CANCELLED} of a buy-side order correctly reverts cash and reserved
   const initialState = Map({
     cash: 0,
     reservedCash: 10010,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
@@ -170,21 +141,14 @@ test(`${ORDER_FILLED}, sell-side, should increase cash and commission, and decre
   const initialState = Map({
     cash: -10,
     reservedCash: 10,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 9990,
     reservedCash: 0,
-    commission: 10,
-    history: List([Map({
-      cash: -10,
-      reservedCash: 10,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 10
   })
 
   t.true(is(actual, expect))
@@ -206,21 +170,14 @@ test(`${ORDER_FILLED}, buy-side, should increase commission and decrease reserve
   const initialState = Map({
     cash: 0,
     reservedCash: 10010,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 0,
     reservedCash: 0,
-    commission: 10,
-    history: List([Map({
-      cash: 0,
-      reservedCash: 10010,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 10
   })
 
   t.true(is(actual, expect))
@@ -242,21 +199,14 @@ test(`${ORDER_FILLED}, buy-side, partial fill, should increase commission and de
   const initialState = Map({
     cash: 0,
     reservedCash: 10010,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 0,
     reservedCash: 5005,
-    commission: 5,
-    history: List([Map({
-      cash: 0,
-      reservedCash: 10010,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 5
   })
 
   t.true(is(actual, expect))
@@ -278,21 +228,14 @@ test(`${ORDER_FILLED}, sell-side, partial fill, increase cash and commission, an
   const initialState = Map({
     cash: -10,
     reservedCash: 10,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 4990,
     reservedCash: 5,
-    commission: 5,
-    history: List([Map({
-      cash: -10,
-      reservedCash: 10,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 5
   })
 
   t.true(is(actual, expect))
@@ -314,21 +257,14 @@ test(`${ORDER_FILLED}, buy-side, better price, should increase commission and de
   const initialState = Map({
     cash: 0,
     reservedCash: 10010,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 0,
     reservedCash: 0,
-    commission: 9,
-    history: List([Map({
-      cash: 0,
-      reservedCash: 10010,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 9
   })
 
   t.true(is(actual, expect))
@@ -350,48 +286,15 @@ test(`${ORDER_FILLED}, sell-side, better price, increase cash and commission, an
   const initialState = Map({
     cash: -10,
     reservedCash: 10,
-    commission: 0,
-    history: List()
+    commission: 0
   })
 
   const actual = reducer(initialState, action)
   const expect = Map({
     cash: 10979,
     reservedCash: 0,
-    commission: 11,
-    history: List([Map({
-      cash: -10,
-      reservedCash: 10,
-      commission: 0,
-      timestamp: 100
-    })])
+    commission: 11
   })
 
   t.true(is(actual, expect))
-})
-
-test(`update history on ${BAR_RECEIVED}`, (t) => {
-  const action = {
-    type: BAR_RECEIVED,
-    payload: {
-      identifier: 'MSFT',
-      open: 110,
-      high: 120,
-      low: 90,
-      close: 100,
-      timestamp: 200
-    }
-  }
-
-  const actual = reducer(undefined, action).get('history')
-  const expected = List([
-    Map({
-      cash: 0,
-      reservedCash: 0,
-      commission: 0,
-      timestamp: 200
-    })
-  ])
-
-  t.true(is(actual, expected))
 })
